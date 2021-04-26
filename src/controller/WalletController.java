73400ed63224735c9e7b36859ad8292378e9f549
mgKang3646 <lordofkangs@naver.com>
2021-04-26 16:33:00 +0900

    코인 거래 UI 추가

--- src/controller/WalletController.java
+++ src/controller/WalletController.java
@@ -1,11 +1,20 @@
 package controller;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import model.PeerModel;
 
-public class WalletController {
+public class WalletController implements Initializable{
 
 	@FXML TextField skTextField;
 	@FXML TextField pkTextField;
@@ -13,14 +22,35 @@ public class WalletController {
 	@FXML Button wireButton;
 	private PeerModel peerModel = null;
 	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		skTextField.setEditable(false);
+		pkTextField.setEditable(false);
+		balanceTextField.setEditable(false);
+	}
+	
 	public void setPeerModel(PeerModel peerModel) {
 		this.peerModel = peerModel;
 		skTextField.setText(peerModel.walletModel.getPrivateKey());
 		pkTextField.setText(peerModel.walletModel.getPublicKey());
 	}
 	
-	public void wire() {
+	public void wire() throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/wire.fxml"));
+		Parent root = loader.load();
+		WireController wc = loader.getController();
+		wc.setPeerModel(peerModel);
+		
+		Scene scene = new Scene(root);
+		Stage stage = new Stage();
+		stage.setScene(scene);
+		stage.setX(skTextField.getScene().getWindow().getX()+220);
+		stage.setY(skTextField.getScene().getWindow().getY());
+		stage.show();
+		
 		
 	}
+
+
 	
 }
---
+++ src/controller/WireController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import model.PeerModel;
+
+public class WireController implements Initializable{
+
+	@FXML TextField senderTextField;
+	@FXML TextField receiverTextField;
+	@FXML TextField balanceTextField;
+	@FXML Button wireButton;
+	private PeerModel peerModel = null;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		senderTextField.setEditable(false);
+	}
+	
+	
+	public void setPeerModel(PeerModel peerModel) {
+		this.peerModel = peerModel;
+		senderTextField.setText(peerModel.walletModel.getPublicKey());
+	}
+	
+	public void wire() {
+		
+	}
+
+	
+}
